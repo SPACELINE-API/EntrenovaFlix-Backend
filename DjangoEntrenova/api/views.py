7ae550cc6a36024cb5fd57bad48fc1defcf2ef9b
Vlopes7 <viniciuslopes242005@gmail.com>
2025-10-09 20:29:45 -0300

    feat(SPACE-166): implementação de modelo de IA separadamente

--- DjangoEntrenova/api/views.py
+++ DjangoEntrenova/api/views.py
@@ -1,28 +1,23 @@
-from django.shortcuts import render
+# api/views.py
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .supabase_client import supabase
-import os
-import google.generativeai as genai
 from rest_framework import status
-from django.conf import settings
 from rest_framework.permissions import AllowAny 
+from .ai_service import gemini_service
 
 class ChatbotView(APIView):
-    permission_classes = [AllowAny] #Qualquer um pode acessar isso, deve ser mudado depois
+    permission_classes = [AllowAny] #Necessário mudança, qualquer um tem acesso
 
     def post(self, request):
-        api_key = os.getenv("GEMINI_API_KEY")
-        if not api_key:
+        if not gemini_service:
             return Response(
-                {"error": "A chave da API do Gemini não foi configurada no servidor"},
+                {"error": "O serviço de IA não está disponível devido a um erro de configuração."},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
-        
-        genai.configure(api_key=api_key)
 
         user_message = request.data.get('message')
-        history_from_frontend = request.data.get('history', [])
+        history = request.data.get('history', [])
 
         if not user_message:
             return Response(
@@ -32,27 +27,15 @@ class ChatbotView(APIView):
         
         system_prompt = "Seu objetivo é ser um assistente amigável e educado, seu objetivo é extrair todas as informações da empresa de quem está digitando, como quantidade de funcionários, porte de empresa, cargo de quem está digitando..."
         
-        model = genai.GenerativeModel(
-            model_name="gemini-2.0-flash-001",
-            system_instruction=system_prompt
-        )
-
-        api_history = []
-        for message in history_from_frontend:
-            if message.get('role') and message.get('text'):
-                 api_history.append({
-                    'role': message['role'],
-                    'parts': [{'text': message['text']}]
-                })
-
         try:
-            chat_session = model.start_chat(history=api_history)
+            chat_session = gemini_service.start_chat_session(system_prompt, history)
             response = chat_session.send_message(user_message)
             
             return Response(
                 {'reply': response.text},
                 status=status.HTTP_200_OK
             )
+        
         except Exception as e:
             print(f"Erro na API do Gemini: {e}")
             return Response(
