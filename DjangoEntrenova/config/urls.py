a2760f3055884f636f3a72cfb2fb0e7ce69c76f6
juliaprianti06 <juliaprianti3382@hotmail.com>
2025-10-18 16:54:40 -0300

    feat(IA):adicionando estrutura de próximos passos com IA integrada

--- DjangoEntrenova/config/urls.py
+++ DjangoEntrenova/config/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api.views import ChatbotView, funcionarios
 from api.views import DiagnosticAIView
+from api.views import ProximosPassosView
 
 
 urlpatterns = [
@@ -10,5 +11,6 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('api/chatbot/', ChatbotView.as_view(), name='chatbot'),
     path('api/diagnostico/avaliar', DiagnosticAIView.as_view(), name='avaliar-diagnostico'),
+    path('api/proximos-passos/', ProximosPassosView.as_view(), name='proximos_passos'),
     path('api/accounts/funcionario', funcionarios.as_view(), name='funcionarios')
 ]
