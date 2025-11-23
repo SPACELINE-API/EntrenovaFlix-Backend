fd76b5a2b82f2b9b853056d176bed7e6277013ea
Vlopes7 <viniciuslopes242005@gmail.com>
2025-11-23 11:57:28 -0300

    fix: alteração de prompt do chatbot e algumas rotas

--- DjangoEntrenova/config/settings.py
+++ DjangoEntrenova/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from datetime import timedelta
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -155,4 +156,5 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
   "USER_ID_FIELD": "id",
   "USER_ID_CLAIM": "user_id",
+  "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60)
 }
